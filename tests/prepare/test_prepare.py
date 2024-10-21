ddd271b71ec30d07fcaa7c3bbdfb0071f7094ff4
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-10-21 15:35:45 +0530

    refactor code

--- tests/prepare/test_prepare.py
+++ tests/prepare/test_prepare.py
@@ -1,3 +1,4 @@
+import tempfile
 from pathlib import Path
 
 from bo_rag_prep_tool.prepare import prepare
@@ -8,12 +9,14 @@ def test_prepare():
     data = Path(__file__).parent / "data"
     opf_path = data / "I46409446.opf"
 
-    output_path = Path(__file__).parent / "output"
-    json_output_path = prepare(opf_path, output_path)
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        output_path = Path(tmpdirname)
 
-    expected_output = read_json(data / "expected_output.json")
+        json_output_path = prepare(opf_path, output_path)
 
-    json_output = read_json(json_output_path)
-    assert len(json_output) == len(expected_output)
-    for json_data in json_output:
-        assert json_data in expected_output
+        expected_output = read_json(data / "expected_output.json")
+
+        json_output = read_json(json_output_path)
+        assert len(json_output) == len(expected_output)
+        for json_data in json_output:
+            assert json_data in expected_output
