ad951a28366ec51cae1705170821cbbeca0379f1
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-10-21 15:34:21 +0530

    modify test_prepare

--- tests/prepare/test_prepare.py
+++ tests/prepare/test_prepare.py
@@ -1,4 +1,3 @@
-import tempfile
 from pathlib import Path
 
 from bo_rag_prep_tool.prepare import prepare
@@ -9,10 +8,12 @@ def test_prepare():
     data = Path(__file__).parent / "data"
     opf_path = data / "I46409446.opf"
 
-    with tempfile.TemporaryDirectory() as tmpdirname:
+    output_path = Path(__file__).parent / "output"
+    json_output_path = prepare(opf_path, output_path)
 
-        output_path = Path(tmpdirname)
-        json_output_path = prepare(opf_path, output_path)
+    expected_output = read_json(data / "expected_output.json")
 
-        expected_output = data / "expected_output.json"
-        assert read_json(json_output_path) == read_json(expected_output)
+    json_output = read_json(json_output_path)
+    assert len(json_output) == len(expected_output)
+    for json_data in json_output:
+        assert json_data in expected_output
