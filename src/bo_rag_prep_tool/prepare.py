1eb7d120514eab76d25ef5a8a4325195e9ba8ed6
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-10-22 12:58:07 +0530

    extract text from pdf

--- src/bo_rag_prep_tool/prepare.py
+++ src/bo_rag_prep_tool/prepare.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+from typing import Dict
 
+import fitz
 import yaml
 
 from bo_rag_prep_tool.utils import write_json
@@ -39,3 +41,18 @@ def prepare_from_opf(opf_path: Path, output_path: Path):
     json_output_path = output_path / f"{pecha_id}.json"
     write_json(json_output_path, book_metadata)
     return json_output_path
+
+
+def extract_text_from_pdf_file(pdf_file_path: Path) -> Dict[int, str]:
+    """Reads the content of a PDF file using PyMuPDF."""
+    text = {}
+
+    try:
+        pdf_document = fitz.open(pdf_file_path)
+        for no in range(len(pdf_document)):
+            page = pdf_document.load_page(no)
+            text[no + 1] = page.get_text() if page.get_text() else ""
+        return text
+    except Exception as e:
+        print(f"Failed to read PDF file {pdf_file_path}: {e}")
+        return {}
